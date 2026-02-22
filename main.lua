8f96ecb31b5096e308fa85db24068336829ccd42
Cyber Squib <peterhodgden@icloud.com>
2026-02-22 08:47:08 -0600

    implemented graphics

--- main.lua
+++ main.lua
@@ -7,10 +7,38 @@ _gfx.table = love.graphics.newImage("rsrc/table.png")
 
 love.window.setMode(1920,1080,{centered=true,borderless=true})
 
+
 function love.update()
 
 end
 
 function love.draw()
-  love.graphics.draw(_gfx.chair, 300, 200)
+  love.graphics.clear(1,1,1,1)
+  love.graphics.translate(1920/2,1080*5/8)
+  love.graphics.draw(_gfx.table,
+      -_gfx.chair:getWidth()*1.25/2-25,
+      -_gfx.chair:getHeight()*1.25/2-100,
+      0,1.25,1.25)
+  love.graphics.draw(_gfx.chair,
+      -_gfx.chair:getWidth()/2-300,
+      -_gfx.chair:getHeight()/2)
+  love.graphics.draw(_gfx.chair,
+      _gfx.chair:getWidth()/2+300,
+      -_gfx.chair:getHeight()/2,
+      0,-1,1)
+  love.graphics.draw(_gfx.mouse,
+      -_gfx.mouse:getWidth()*1.5/2-300,
+      -_gfx.mouse:getHeight()*1.5/2-200,
+      0,1.5,1.5)
+  love.graphics.draw(_gfx.mouse,
+      _gfx.mouse:getWidth()*1.5/2+300,
+      -_gfx.mouse:getHeight()*1.5/2-200,
+      0,-1.5,1.5)
+  love.graphics.draw(_gfx.dialogue,
+      -_gfx.chair:getWidth()*1/2-25,
+      -_gfx.chair:getHeight()*1/2-450,
+      0,1,1)
+  --love.graphics.draw(_gfx.chair,500,200,0,-1,1)
 end
+
+-- working
